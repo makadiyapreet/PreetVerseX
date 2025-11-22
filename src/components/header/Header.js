7522a280c4dcfdfffbfcf856ce3746a2f0002cc5
makadiyapreet <makadiyapreet1@gmail.com>
2025-11-22 11:17:14 +0530

    Fixed mobile layout for contact page

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,11 +6,13 @@ import { greeting, settings } from "../../portfolio.js";
 import SeoHeader from "../seoHeader/SeoHeader";
 
 const onMouseEnter = (event, color) => {
-  event.target.style.backgroundColor = color;
+  const el = event.target;
+  el.style.backgroundColor = color;
 };
 
 const onMouseOut = (event) => {
-  event.target.style.backgroundColor = "transparent";
+  const el = event.target;
+  el.style.backgroundColor = "transparent";
 };
 
 class Header extends Component {
@@ -21,97 +23,115 @@ class Header extends Component {
     return (
       <Fade top duration={1000} distance="20px">
         <SeoHeader />
-        <header className="header" style={{ background: theme.body }}>
-          <NavLink to={link} tag={Link} className="logo">
-            <span style={{ color: theme.text }}>&lt;</span>
-            <span className="logo-name" style={{ color: theme.text }}>
-              {greeting.logo_name}
-            </span>
-            <span style={{ color: theme.text }}>/&gt;</span>
-          </NavLink>
+        <div>
+          <header className="header">
+            <NavLink to={link} tag={Link} className="logo">
+              <span style={{ color: theme.text }}>&lt;</span>
+              <span className="logo-name" style={{ color: theme.text }}>
+                {greeting.logo_name}
+              </span>
+              <span style={{ color: theme.text }}>/&gt;</span>
+            </NavLink>
 
-          {/* MOBILE MENU BUTTON */}
-          <input className="menu-btn" type="checkbox" id="menu-btn" />
-          <label className="menu-icon" htmlFor="menu-btn">
-            <span className="navicon"></span>
-          </label>
+            <input className="menu-btn" type="checkbox" id="menu-btn" />
+            <label className="menu-icon" htmlFor="menu-btn">
+              <span className="navicon"></span>
+            </label>
 
-          {/* MENU LINKS */}
-          <ul className="menu" style={{ background: theme.body }}>
-            <li>
-              <NavLink
-                to="/home"
-                style={{ color: theme.text }}
-                activeStyle={{ fontWeight: "bold" }}
-                onMouseEnter={(event) => onMouseEnter(event, theme.highlight)}
-                onMouseOut={onMouseOut}
-              >
-                Home
-              </NavLink>
-            </li>
-
-            <li>
-              <NavLink
-                to="/education"
-                activeStyle={{ fontWeight: "bold" }}
-                style={{ color: theme.text }}
-                onMouseEnter={(event) => onMouseEnter(event, theme.highlight)}
-                onMouseOut={onMouseOut}
-              >
-                Education
-              </NavLink>
-            </li>
-
-            <li>
-              <NavLink
-                to="/experience"
-                activeStyle={{ fontWeight: "bold" }}
-                style={{ color: theme.text }}
-                onMouseEnter={(event) => onMouseEnter(event, theme.highlight)}
-                onMouseOut={onMouseOut}
-              >
-                Experience
-              </NavLink>
-            </li>
-
-            <li>
-              <NavLink
-                to="/projects"
-                activeStyle={{ fontWeight: "bold" }}
-                style={{ color: theme.text }}
-                onMouseEnter={(event) => onMouseEnter(event, theme.highlight)}
-                onMouseOut={onMouseOut}
-              >
-                Projects
-              </NavLink>
-            </li>
-
-            <li>
-              <NavLink
-                to="/contact"
-                activeStyle={{ fontWeight: "bold" }}
-                style={{ color: theme.text }}
-                onMouseEnter={(event) => onMouseEnter(event, theme.highlight)}
-                onMouseOut={onMouseOut}
-              >
-                Contact Me
-              </NavLink>
-            </li>
-            {/*
+            <ul className="menu" style={{ backgroundColor: theme.body }}>
               <li>
+                <NavLink
+                  to="/home"
+                  tag={Link}
+                  activeStyle={{ fontWeight: "bold" }}
+                  style={{ color: theme.text }}
+                  onMouseEnter={(event) =>
+                    onMouseEnter(event, theme.highlight)
+                  }
+                  onMouseOut={onMouseOut}
+                >
+                  Home
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="/education"
+                  tag={Link}
+                  activeStyle={{ fontWeight: "bold" }}
+                  style={{ color: theme.text }}
+                  onMouseEnter={(event) =>
+                    onMouseEnter(event, theme.highlight)
+                  }
+                  onMouseOut={onMouseOut}
+                >
+                  Education
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="/experience"
+                  tag={Link}
+                  activeStyle={{ fontWeight: "bold" }}
+                  style={{ color: theme.text }}
+                  onMouseEnter={(event) =>
+                    onMouseEnter(event, theme.highlight)
+                  }
+                  onMouseOut={onMouseOut}
+                >
+                  Experience
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="/projects"
+                  tag={Link}
+                  activeStyle={{ fontWeight: "bold" }}
+                  style={{ color: theme.text }}
+                  onMouseEnter={(event) =>
+                    onMouseEnter(event, theme.highlight)
+                  }
+                  onMouseOut={onMouseOut}
+                >
+                  Projects
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="/contact"
+                  tag={Link}
+                  activeStyle={{ fontWeight: "bold" }}
+                  style={{ color: theme.text }}
+                  onMouseEnter={(event) =>
+                    onMouseEnter(event, theme.highlight)
+                  }
+                  onMouseOut={onMouseOut}
+                >
+                  Contact Me
+                </NavLink>
+              </li>
+
+              {/* <li>
                 <NavLink
                   to="/opensource"
                   tag={Link}
                   activeStyle={{ fontWeight: "bold" }}
                   style={{ color: theme.text }}
-                  onMouseEnter={(event) => onMouseEnter(event, theme.highlight)}
-                  onMouseOut={(event) => onMouseOut(event)}
+                  onMouseEnter={(event) =>
+                    onMouseEnter(event, theme.highlight)
+                  }
+                  onMouseOut={onMouseOut}
                 >
                   Open Source
                 </NavLink>
               </li> */}
-          </ul>
-        </header>
+              
+            </ul>
+          </header>
+        </div>
       </Fade>
     );
   }
